503073a6ed1b2425a0abf59b5d9cc2afd2d1b7ea
Christian Garcia <christian.garcia@origamibits.co.uk>
2020-04-13 18:57:18 +0100

    FAIL: StartingPointParser Test suite

---
+++ app/src/main/java/uk/co/origamibits/redbadger/reader/StartingPointParser.kt
@@ -0,0 +1,10 @@
+package uk.co.origamibits.redbadger.reader
+
+import uk.co.origamibits.redbadger.model.StartingPoint
+
+class StartingPointParser {
+    fun parse(line: String): StartingPoint? {
+        TODO()
+    }
+
+}
---
+++ app/src/test/java/uk/co/origamibits/redbadger/reader/StartingPointParserTest.kt
@@ -0,0 +1,39 @@
+package uk.co.origamibits.redbadger.reader
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Before
+import org.junit.Test
+import uk.co.origamibits.redbadger.model.Orientation
+import uk.co.origamibits.redbadger.model.StartingPoint
+
+class StartingPointParserTest {
+
+    private lateinit var parser: StartingPointParser
+
+    @Before
+    fun setUp() {
+        parser = StartingPointParser()
+    }
+
+    @Test
+    fun `given entry with missing orientation, when read, then return null`() {
+        assertThat(parser.parse("1 1")).isNull()
+    }
+
+    @Test
+    fun `given entry with negative x, when read, then return null`() {
+        assertThat(parser.parse("-1 1 E")).isNull()
+    }
+
+    @Test
+    fun `given entry with negative y, when read, then return null`() {
+        assertThat(parser.parse("1 -1 E")).isNull()
+    }
+
+    @Test
+    fun `given entry with valid values, when read, then return null`() {
+        assertThat(parser.parse("1 1 E")).isEqualTo(
+            StartingPoint(1, 1, Orientation.E)
+        )
+    }
+}
