9275f24f1c4c171a56d6efc2044346385691e958
Christian Garcia <christian.garcia@origamibits.co.uk>
2020-04-14 00:22:14 +0100

    Delete Unused class

--- app/src/main/java/uk/co/origamibits/redbadger/model/Cell.kt
+++ app/src/main/java/uk/co/origamibits/redbadger/model/Cell.kt
@@ -3,5 +3,4 @@ package uk.co.origamibits.redbadger.model
 sealed class Cell {
     object Empty : Cell()
     object ScentOfLostRobot : Cell()
-    data class Robot(val orientation: Orientation) : Cell()
 }
