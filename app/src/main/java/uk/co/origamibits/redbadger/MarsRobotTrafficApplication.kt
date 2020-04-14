ce0543cf81fac2f868ac7920de7e0f64fc5b27ce
Christian Garcia <christian.garcia@origamibits.co.uk>
2020-04-14 16:58:42 +0100

    Experimental warnings

--- app/src/main/java/uk/co/origamibits/redbadger/MarsRobotTrafficApplication.kt
+++ app/src/main/java/uk/co/origamibits/redbadger/MarsRobotTrafficApplication.kt
@@ -4,6 +4,7 @@ import android.app.Application
 import dagger.android.AndroidInjector
 import dagger.android.DispatchingAndroidInjector
 import dagger.android.HasAndroidInjector
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import timber.log.Timber
 import uk.co.origamibits.redbadger.di.ApplicationModule
 import uk.co.origamibits.redbadger.di.DaggerApplicationComponent
@@ -13,6 +14,7 @@ class MarsRobotTrafficApplication : Application(), HasAndroidInjector {
     @Inject
     lateinit var dispatchingAndroidInjector: DispatchingAndroidInjector<Any>
 
+    @ExperimentalCoroutinesApi
     @ExperimentalStdlibApi
     override fun onCreate() {
         super.onCreate()
